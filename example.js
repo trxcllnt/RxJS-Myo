c34090310a6f1a24f91f7c17c3b958ff49252014
Paul Taylor <ptaylor@netflix.com>
2015-01-15 02:24:04 -0800

    Fixes the event renames in Arm.

--- example.js
+++ example.js
@@ -1,18 +1,23 @@
-var inspect = require("util").inspect,
-    Rx = require("rx"),
+var Rx = require("rx"),
     ws = require("ws"),
     Myo = require("./index"),
-    Hub = Myo(ws.connect);
+    Hub = Myo(ws.connect || ws),
+    inspect = require("util").inspect,
+    emg = Hub
+        .Events()
+        .groupByArm()
+        .flatMap(function(Arm) {
+            return Arm
+                .setEMG({"type": "enabled"})
+                .isEMG();
+        });
 
-Hub .Events()
-    .groupByArm()
-    .flatMap(function(Arm) {
-        return Arm
-            .setEMG({"type": "enabled"})
-            .emg();
-    })
-    .forEach(
+if(document) {
+    var canvas = document.createElement("canvas");
+} else {
+    emg.forEach(
         function(xs) { console.log(inspect(xs, {depth:null})); },
-        function(er) { console.error(er); },
+        function(er) { console.error(er.stack || er.toString()); },
         function(  ) { console.log("completed"); }
     );
+}
