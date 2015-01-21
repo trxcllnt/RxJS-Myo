6594558a59459ed52c6161cc07aea84d36ac3e42
Paul Taylor <ptaylor@netflix.com>
2015-01-20 16:10:52 -0800

    Updates example vis.

--- example.js
+++ example.js
@@ -12,6 +12,8 @@ window && (window.onload = function() {
     var body   = document.getElementsByTagName("body")[0];
     body.style.margin = 0;
     body.style.padding = 0;
+    body.style.position = "absolute";
+    body.style.left = body.style.top = body.style.right = body.style.bottom = 0;
     
     emgs.scan([body], function(xs, Arm) {
         var body = xs[0],
@@ -19,16 +21,18 @@ window && (window.onload = function() {
             canvas, i = -1, n = canvases.length + 1;
         while(++i < n) {
             canvas = canvases[i] || body.appendChild(document.createElement("canvas"));
-            canvas.style.display = "inline-block";
-            canvas.style.verticalAlign = "middle";
-            canvas.width = body.offsetWidth;
-            canvas.height = body.offsetHeight * 0.5;
+            canvas.style.minWidth = "25%";
+            canvas.style.width = canvas.width = body.offsetWidth;
+            canvas.style.height = canvas.height = body.offsetHeight * 0.5;
+            canvas.style.position = "absolute";
+            canvas.style.top = "50%";
+            canvas.style.marginTop = (body.offsetHeight * 0.25 * -1) + "px";
         }
         return [body, Arm];
     })
     .flatMap(function(xs) {
         
-        var body     = xs[0], Arm = xs[1],
+        var body     = xs[0], Arm = xs[1], image,
             canvases = body.getElementsByTagName("canvas"),
             canvas   = canvases[canvases.length - 1],
             context  = canvas.getContext("2d"),
@@ -44,23 +48,25 @@ window && (window.onload = function() {
             .map(function(x, i) {
                 
                 var emgs = x.emg,
-                    j = -1, n = emgs.length,
+                    j = 0, n = emgs.length,
                     width = canvas.offsetWidth,
                     height = canvas.offsetHeight / n;
                 
                 if(i >= width) {
                     i = width;
-                    context.drawImage(canvas, -1, 0);
+                    image = context.getImageData(0, 0, width, height * n);
+                    context.clearRect(0, 0, width, height * n);
+                    context.putImageData(image, -1, 0);
                     context.fillRect(width - 1, 0, width, height * n);
                 } else {
                     i += 1;
                 }
                 
-                while(++j < n) {
-                    context.strokeStyle = "black";
+                while(++j <= n) {
+                    context.strokeStyle = "rgba(0, 0, 0, " + (j / n) + ")";
                     context.beginPath();
-                    context.moveTo(i - 1, height * j + (xs[j] / n));
-                    context.lineTo(i, height * j + (emgs[j] / n));
+                    context.moveTo(i - 1, (height * j) - (height * 0.5) + (xs[j - 1] / n * 2));
+                    context.lineTo(i, (height * j) - (height * 0.5) + (emgs[j - 1] / n * 2));
                     context.closePath();
                     context.stroke();
                 }
